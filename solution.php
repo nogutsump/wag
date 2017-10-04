b5c1e631d7c1296edb1112d56846412bca05f0c8
Melvin Tercan <melvin@wagwalking.com>
2017-10-04 12:13:42 -0700

    initial commit

---
+++ solution.php
@@ -0,0 +1,22 @@
+<?php
+// YOUR NAME AND EMAIL GO HERE
+
+function parse_request($request, $secret)
+{
+    // YOUR CODE GOES HERE
+}
+
+function dates_with_at_least_n_scores($pdo, $n)
+{
+    // YOUR CODE GOES HERE
+}
+
+function users_with_top_score_on_date($pdo, $date)
+{
+    // YOUR CODE GOES HERE
+}
+
+function dates_when_user_was_in_top_n($pdo, $user_id, $n)
+{
+    // YOUR CODE GOES HERE
+}
